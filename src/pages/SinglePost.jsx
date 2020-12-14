d9e33fb0ba9b3e28984829503864250ab99a2bb4
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-12-14 11:57:27 -0800

    adjusts profile img size

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -66,14 +66,14 @@ function SinglePost(props) {
     postMarkup = (
       <Grid>
         <Grid.Row>
-          <Grid.Column width={2}>
+          <Grid.Column width={5}>
             <Image
               src={userName === "Brandon_LeBoeuf" ? "https://avatars3.githubusercontent.com/u/54607186?s=400&u=6b83490323e35808fae23a82d4cef7c6de5ee7cf&v=4" : `https://randomuser.me/api/portraits/med/men/${imgNum}.jpg`}
               size="small"
               floated="right"
             />
           </Grid.Column>
-          <Grid.Column width={12}>
+          <Grid.Column width={13}>
             <Card fluid>
               <Card.Content>
                 <Card.Header>{userName}</Card.Header>
